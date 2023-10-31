fd19e3a0ed9612f73c1da009455d21bd523b7afd
Raghav Bajaj <bajajraghav9@gmail.com>
2023-10-31 15:15:32 +0530

    Model update

--- personal_portfolio/models.py
+++ personal_portfolio/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+import pytz
 
 
 # Create your models here.
@@ -9,17 +10,16 @@ class FormData(models.Model):
     email = models.CharField(max_length=50, blank=False, null=False, default="xyz@abc.com")
     subject = models.CharField(max_length=100, blank=True, default='')
     message = models.TextField(max_length=2500, blank=False, null=False, default="Something")
-    date = models.DateField(auto_now_add=True)
-    time = models.TimeField(auto_now_add=True)
+    date = models.DateField()
+    time = models.TimeField()
     objects = models.Manager()
 
     def save(self, *args, **kwargs):
         # Set the default values for Date and Time fields to the current date and time in IST
-        ist_timezone = timezone.get_fixed_timezone(330)  # 330 minutes is the offset for IST
-        current_datetime = timezone.now()
-        ist_time = current_datetime.astimezone(ist_timezone)
-        self.date = ist_time.date()
-        self.time = ist_time.time()
+        ist_timezone = pytz.timezone('Asia/Kolkata')
+        current_datetime = timezone.now().astimezone(ist_timezone)
+        self.date = current_datetime.date()
+        self.time = current_datetime.time()
         super().save(*args, **kwargs)
 
     def __str__(self):
