709026990eb9696371208610f6aec30526b500a7
RRB230899 <bajajraghav9@gmail.com>
2025-09-10 19:43:47 -0400

    Vercel Config Changes for CSS

--- build_files.sh
+++ build_files.sh
@@ -1,16 +1,7 @@
 echo "BUILD STARTED"
 
-# 1. Use venv to isolate environment
-python3 -m venv venv
-source venv/bin/activate
-
-# 2. Upgrade pip
 python -m pip install --upgrade pip
-
-# 3. Install requirements
 pip install -r requirements.txt
-
-# 4. Collect static files
 python manage.py collectstatic --noinput
 
 echo "BUILD ENDED"
