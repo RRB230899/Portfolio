d8061e504f73a4846afa5cadf8cdc81bde6a60e6
RRB230899 <bajajraghav9@gmail.com>
2025-09-10 20:29:07 -0400

    lk

--- build_files.sh
+++ build_files.sh
@@ -1,22 +1,2 @@
 #!/bin/bash
-# Exit immediately if a command exits with a non-zero status
-set -e
-
-# Clean previous build
-rm -rf .vercel/output
-
-# Create output folder
-mkdir -p .vercel/output
-
-# Collect static files into output folder
 python manage.py collectstatic --noinput
-
-# Copy Django project and apps to output folder
-cp -r portfolio .vercel/output/
-cp -r personal_portfolio .vercel/output/
-
-# Copy wsgi.py to the output folder (Vercel Lambda entry point)
-cp portfolio/wsgi.py .vercel/output/
-
-# Copy any other necessary files (optional)
-cp manage.py .vercel/output/
